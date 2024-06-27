e549397acec42a4f6bd3ea7bd8869ace65432c7d
bikramia <bikram.rhce@gmail.com>
2024-06-27 12:32:48 -0400

    Testing Strings

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,18 +1,12 @@
 const lib = require('../lib');
 
-describe('absolute', () =>{
-    it('should return a positive number if input is positive', () => {
-        const result = lib.absolute(1);
-        expect(result).toBe(1);
-    });
-    
-    it('should return a positive number if input is negative', () => {
-        const result = lib.absolute(-1);
-        expect(result).toBe(1);
-    });
-    
-    it('should return 0 number if input is 0', () => {
-        const result = lib.absolute(0);
-        expect(result).toBe(0);
+describe('absolute', () => {
+});
+
+describe('greet', () => {
+    it('should return the  greeting message', () => {
+        const result = lib.greet('Bikram');
+        expect(result).toMatch(/Bikram/);
+        expect(result).toContain('Bikram');
     });
 });
